1911b3ea366bdddda19b42c9c1519c92f21c3efc
Ian Bush <ianfracnisbush@gmail.com>
2019-08-24 10:46:58 -0400

    Adding some text to container

--- flutter_app/flutter_app/lib/main.dart
+++ flutter_app/flutter_app/lib/main.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 void main () {
  runApp(
    Container (
-     color: Colors.lime
-   )
+     color: Colors.lime,
+     child: Center(
+       child: Text("Hello from flutter", textDirection: TextDirection.ltr,style: TextStyle(fontSize: 32.0, color: Colors.black),
+   ),
+     ))
  );
 }
