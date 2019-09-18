6977b6954397aeaeaed225d8912b55a9f763b6e2
Ian Bush <ianfracnisbush@gmail.com>
2019-09-18 13:41:26 -0400

    Statefull widget changes color

--- flutter_app/flutter_app/lib/main.dart
+++ flutter_app/flutter_app/lib/main.dart
@@ -2,11 +2,37 @@ import 'package:flutter/material.dart';
 
 void main () {
  runApp(
-   Container (
-     color: Colors.lime,
-     child: Center(
-       child: Text("Hello from flutter", textDirection: TextDirection.ltr,style: TextStyle(fontSize: 32.0, color: Colors.black),
-   ),
-     ))
+     MyStatefulWidget()
  );
 }
+
+class MyStatefulWidget extends StatefulWidget {
+  @override
+  _MyStatefulWidgetState createState() => new _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  var myColor=Colors.red;
+  changeColor()
+  {
+    myColor=Colors.blue;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home : Container(
+          color:myColor,
+          child: Center(
+            child:RaisedButton(
+              child: Text("click", textDirection: TextDirection.ltr,),
+              onPressed:(){
+                changeColor();
+              },
+            ),//Raised button
+          ),//Center
+
+        )
+    );
+  }
+}
+
